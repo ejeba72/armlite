cf6130dbfa5946cf355b69efa78d258a00450dd3
Emmanuel Eni <eejeba@gmail.com>
2023-07-27 23:13:34 +0100

    corrections

--- src/exercises/5-indirect-indexed-addressing/28b.exercise.s
+++ src/exercises/5-indirect-indexed-addressing/28b.exercise.s
@@ -1,8 +1,8 @@
 // Define & initialise registers
-MOV R0, #arrayData //Address of array data
-LDR R1, arrayData
-LSL R1, R1, #2 //Multiply this by 4 (for byte count) 
-MOV R2, #0 //outerLoop counter initialized
+  MOV R0, #arrayData //Address of array data
+  LDR R1, arrayLength
+  LSL R1, R1, #2 //Multiply this by 4 (for byte count) 
+  MOV R2, #0 //outerLoop counter initialized
 // R3 innerLoop counter
 // R4 length of innerLoop
 // R5 spare index into array
@@ -10,41 +10,42 @@ MOV R2, #0 //outerLoop counter initialized
 // R7 second in pair
 // R8 ?
 startOfOuterLoop:
-MOV R3, #0 //reset inner loop counter to zero
-SUB R4, R1, R2 //set innerloop max to data length - outerLoopCounter... 
-SUB R4, R4, #4 //...minus 4 more
-MOV R8, #0
+  MOV R3, #0 //reset inner loop counter to zero
+  SUB R4, R1, R2 //set innerloop max to data length - outerLoopCounter... 
+  SUB R4, R4, #4 //...minus 4 more
+  MOV R8, #0
 innerLoop:
-LDR R6, [R0+R3] //Load first value from address (base + index) 
-ADD R5, R3, #4 //Generate index for second value in pair
-LDR R7, [R0+R5] //Load second value
-CMP R6, R7 //Compare and swap if appropriate
-BGT swap
-B continueInnerLoop
+  LDR R6, [R0+R3] //Load first value from address (base + index) 
+  ADD R5, R3, #4 //Generate index for second value in pair
+  LDR R7, [R0+R5] //Load second value
+  CMP R6, R7 //Compare and swap if appropriate
+  BGT swap
+  B continueInnerLoop
 swap:
-   STR R7, [R0+R3]
-   STR R6, [R0+R5]
-   MOV R8, #1
+  STR R7, [R0+R3]
+  STR R6, [R0+R5]
+  MOV R8, #1
 continueInnerLoop:
-   ADD R3, R3, #4
-   CMP R3, R4 //Check if reached the end
-   BLT innerLoop
+  ADD R3, R3, #4
+  CMP R3, R4 //Check if reached the end
+  BLT innerLoop
 continueOuterLoop:
-   CMP R8, #0
-   BEQ done
-   ADD R2, R2, #4
-   CMP R2, R1
-   BGT done
-   B startOfOuterLoop
+  CMP R8, #0
+  BEQ done
+  ADD R2, R2, #4
+  CMP R2, R1
+  BGT done
+  B startOfOuterLoop
 done: HALT
-.ALIGN 256 //Just to make data distinct from code in memory view arrayLength: 10
+.ALIGN 256 //Just to make data distinct from code in memory view 
+arrayLength: 10
 arrayData: 9 //1st element
-   8
-   7
-   6
-   5
-   4
-   3
-   2
-   1
-   0 //last element
+  8
+  7
+  6
+  5
+  4
+  3
+  2
+  1
+  0 //last element
